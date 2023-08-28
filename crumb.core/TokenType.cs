c911d0db09f943c2e3d2c60dbb69306390344294
Waters <h2oboi89@gmail.com>
2023-08-28 05:00:59 -0400

    Tokens implemented

---
+++ crumb.core/TokenType.cs
@@ -0,0 +1,20 @@
+﻿namespace crumb.core
+{
+    internal enum TokenType
+    {
+        Assignment,
+        ApplyOpen,
+        ApplyClose,
+        FunctionOpen,
+        FunctionClose,
+        Comma,
+        Arrow,
+        Return,
+        Identifier,
+        Integer,
+        Float,
+        String,
+        End,
+        Start,
+    }
+}
