b7ed735bdb6423af4fb1d271af5b63d90cb21350
Waters <h2oboi89@gmail.com>
2023-08-28 10:35:48 -0400

    AstNode implemented

---
+++ crumb.core/Parsing/OpCodes.cs
@@ -0,0 +1,13 @@
+﻿namespace crumb.core.Parsing;
+internal enum OpCodes
+{
+    Integer,
+    Float,
+    String,
+    Identifier,
+    Assignment,
+    Return,
+    Statement,
+    Application,
+    Function,
+}
