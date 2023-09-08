57d2d72cb2d3724426eb6d953ff6448d3b79591a
Waters <h2oboi89@gmail.com>
2023-09-08 08:15:29 -0400

    added <= and >=

--- Crumb.Core/Evaluating/StandardLibrary/Names.cs
+++ Crumb.Core/Evaluating/StandardLibrary/Names.cs
@@ -29,8 +29,10 @@ internal static class Names
     // comparisons
     public const string Is = "is";
     public const string LessThan = "<";
-    public const string GreaterThan = ">";
+    public const string LessThanOrEqual = "<=";
     public const string Equal = "=";
+    public const string GreaterThanOrEqual = ">=";
+    public const string GreaterThan = ">";
 
     // logic operators^
     public const string Not = "not";
